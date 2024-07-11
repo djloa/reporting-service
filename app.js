8bc89c3d42ed5e5588d599d2f508882b313c047f
daraujo <diogo@kado.money>
2024-07-11 02:12:09 +0100

    update ports

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const limiter = rateLimit({
 app.use(limiter);
 
 // Define a port to listen on
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8080;
 
 
 // Get the MongoDB connection string from environment variables
